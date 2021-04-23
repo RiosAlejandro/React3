beab54401e2c020deaa74d64590fe1ff9836508d
RiosAlejandro <alexandro.aa86@gmail.com>
2021-04-23 13:04:52 -0300

    inicio de proyecto

---
+++ src/Components/Formulario.jsx
@@ -0,0 +1,26 @@
+import React, {Fragment} from 'react';
+
+const Formulario = () => {
+    return ( 
+        <Fragment>
+            <form>
+                <h1>REGISTER</h1>
+                <div className="campoNombre">
+                    <input />
+                    <input />
+                </div>
+                <div className="campoInput">
+                    <input />
+                </div>
+                <div className="campoInput">
+                    <input />
+                </div>
+                <div className="campoInput">
+                    <input />
+                </div>
+            </form>
+        </Fragment>
+     );
+}
+ 
+export default Formulario;
