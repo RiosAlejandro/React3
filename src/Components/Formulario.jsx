7aff76d591676cfeb636fc3ee3ea5f54b36cf312
RiosAlejandro <alexandro.aa86@gmail.com>
2021-11-30 15:59:02 -0300

    avances en validacion de campos formulario

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,32 +1,68 @@
-import React, {Fragment} from 'react';
+import React, { useState } from 'react';
 import './Formulario.css'
 
 const Formulario = () => {
+
+    const [form, setForm] = useState({});
+
+    const handleChange = (e) =>{
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
     return ( 
-        <Fragment>
-            <form>
+        <>
+            <form onSubmit={handleSubmit}>
                 <h1>REGISTER</h1>
                 <div className="campoNombre">
                     <input 
                         placeholder="First name"
+                        type="text"
+                        id="firstName"
+                        name="firstName"
+                        value={form.name}
+                        onChange={handleChange}
                     />
                     <input 
                         placeholder="Last name"
+                        type="text"
+                        id="lastName"
+                        name="lastName"
+                        value={form.name}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="campoInput">
                     <input 
                         placeholder="Email"
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={form.name}
+                        onChange={handleChange}
                     />
                 </div>
                 <div className="campoInput">
                     <input 
                         placeholder="Password"
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={form.name}
+                        onChange={handleChange}
+                        /* poner un patron a la validacion de este campo */
                     />
                 </div>
                 <div className="campoInput">
                     <input 
                         placeholder="Confirm password"
+                        type="password"
                     />
                 </div>
                 <div className="campoRadio">
@@ -37,7 +73,7 @@ const Formulario = () => {
                     <button>REGISTER NOW</button>
                 </div>
             </form>
-        </Fragment>
+        </>
      );
 }
  
